6df9763b3c4ab722ce75c94f436ff1b0236a3441
root <mistral@7ov.ru>
2014-05-24 20:55:26 +0700

    all

---
+++ js/localization/localization.default.js
@@ -0,0 +1,33 @@
+﻿Globalize.addCultureInfo("default", {
+    messages: {
+        "tipCalculator": "Скидульник",
+        "billTotal": "Цена",
+        "tip": "Скидка",
+        "split": "Человеки",
+        "totalToPay": "Всего к оплате:",
+        "totalPerPerson": "Всего на человека:",
+        "totalTip": "Размер скидки:",
+        "tipPerPerson": "Скидка на человека:",
+        "typeHere": "Укажите цену тут",
+        "roundDown": "Round Down",
+        "roundUp": "Round Up"
+    },
+    numberFormat: {
+	",": " ",
+	".": ",",
+	"NaN": "Non Numérique",
+	negativeInfinity: "-Infini",
+	positiveInfinity: "+Infini",
+	percent: {
+	    ",": " ",
+	    ".": ","
+	},
+	currency: {
+	    pattern: ["-n $","n $"],
+	    ",": " ",
+	    ".": ",",
+	    symbol: "р."
+	}
+    }
+
+});
